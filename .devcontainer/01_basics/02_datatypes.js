89bc800c8690d01639f5355ac034c704de892133
aditya120-9 <158483388+aditya120-9@users.noreply.github.com>
2024-03-22 06:45:29 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict" // treat all js code as newer version 
+
+// alert(3+3) //we are use node.js not browser
+
+console.log(3 
+    +
+    3) //code readability should be high
+
+console.log("Aditya")
+
+let name="Aditya" // string datatype
+let age= 18; // number as a dataype
+let isloggedIn= false // boolean datatype
+
+/* 
+DATATYPES
+
+1. number => 2 to the power 53
+2. bigint (for bigger numbers)
+3. string => "" 
+4. boolean => true/false
+5.null = standalone values
+6.undefined =>
+7.symbol => unique
+
+8.object
+*/
+console.log(typeof "Aditya")
+console.log(typeof age)
+console.log(typeof null)
+
+console.log(typeof undefined)
