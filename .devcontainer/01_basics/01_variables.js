00d2eefd2b05038dde5015918425a55e2b284f44
aditya120-9 <158483388+aditya120-9@users.noreply.github.com>
2024-03-22 06:04:54 +0000

    container config changes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId= 1533
+let accountEmail="aditya@google.com"
+var accountPassword="123456"
+accountCity="jaipur"
+let accountState;
+
+//accountId=20//Not allowed
+console.log(accountId);
+
+accountEmail="pratap123@google.com"
+accountPassword="9999"
+accountCity="Bengluru"
+
+console.table([accountId , accountEmail ,accountPassword ,accountCity, accountState])
