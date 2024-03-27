53923ca196fa226a26f1e380c9367642a3ad2bd4
aditya120-9 <158483388+aditya120-9@users.noreply.github.com>
2024-03-27 14:46:19 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score ="Aditya"
+
+console.log(typeof score);// variables having null values are of type of OBJECT
+console.log(typeof (score));
+
+let ValueInNumber= Number(score)
+console.log(typeof (ValueInNumber));
+console.log(ValueInNumber);
+
+
+// "33"=> 33
+//"33abc"=>NaN(not an number)
+// true => 1 ; false => 0;
+
+let isloggedIn = "2"
+let booleanLoggedIn = Boolean(isloggedIn);
+console.log(booleanLoggedIn);
+
+// 1=> true ,0=> false
+//""=false
+//"Aditya"=> true
+
+let someNumber= 33;
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
